8846c20221fadbe75c9908247e9692cbfaabc679
kanak gupta <kanakgupta120804@gmail.com>
2025-04-06 18:00:33 +0530

    commit

---
+++ .next/static/chunks/app_layout_61af54.js
@@ -0,0 +1,18 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_layout_61af54.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_layout_61af54.js",
+  "chunks": [
+    "static/chunks/[root of the server]__a917f3._.css",
+    "static/chunks/node_modules_next_2ec622._.js",
+    "static/chunks/node_modules_@clerk_shared_dist_d5b071._.js",
+    "static/chunks/node_modules_swr_dist_b9da23._.js",
+    "static/chunks/node_modules_@clerk_clerk-react_dist_76e1af._.js",
+    "static/chunks/node_modules_23e771._.js",
+    "static/chunks/_8ae28a._.js",
+    "static/chunks/node_modules_@clerk_nextjs_dist_esm_app-router_f85fd2._.js"
+  ],
+  "source": "dynamic"
+});
